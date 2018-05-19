e3d8d6ea62c0699540645c7841f2c0fa2489ffc6
RAJSHREE GAVEL <rajshreegavel111@gmail.com>
2018-05-19 02:00:09 -0700

    reusability concept

---
+++ Laptop.java
@@ -0,0 +1,13 @@
+
+public class Laptop extends Computer{
+	
+	boolean isTouchScreen;
+	boolean isEmbeddedMouse;
+	
+	void Movable(){
+		System.out.println("It can be moved ! ");
+	}
+	void ShutDownWay(){
+		System.out.println("It can be shuted down by clicking a shut down operation!");
+	}
+}// end class
